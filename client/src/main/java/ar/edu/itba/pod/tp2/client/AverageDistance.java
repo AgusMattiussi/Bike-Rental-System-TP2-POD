9d360cee4cc066d54a39be539e8e5dadff2b836e
unknown <aortu@itba.edu.ar>
2023-10-25 18:58:08 -0300

    Read Csv

--- client/src/main/java/ar/edu/itba/pod/tp2/client/AverageDistance.java
+++ client/src/main/java/ar/edu/itba/pod/tp2/client/AverageDistance.java
@@ -1,15 +1,20 @@
 package ar.edu.itba.pod.tp2.client;
 
+import ar.edu.itba.pod.tp2.model.BikeTrip;
+import ar.edu.itba.pod.tp2.model.Station;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.IMap;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
 public class AverageDistance {
     private static Logger logger = LoggerFactory.getLogger(AverageDistance.class);
-    public static void main(String[] args) {
+    public static void averageClientSolver(HazelcastInstance hazelcastInstance, String n, IMap<Integer, BikeTrip> bikeIMap, IMap<Long, Station> stationIMap) {
         // Example = ./query2 -Daddresses='10.6.0.1:5701' -DinPath=. -DoutPath=. -Dn=4
         logger.info("AverageDistance Client Starting...");
         try {
+            String mapName = "testMap";
+            IMap<Integer, String> testMapFromMember = hazelcastInstance.getMap(mapName);
 
 
         }catch (Exception e) {
