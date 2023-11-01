fa819eeca8b5bc5225c0e65b4304a1c59c957e34
Agustin Mattiussi <amattiussi@itba.edu.ar>
2023-11-01 10:34:53 -0300

    Add compile script

---
+++ compile.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+target_dir="server/target"
+client_dir="client/target"
+temp_dir="tmp"
+
+mvn clean install
+
+mkdir -p "$temp_dir"
+
+cp "$target_dir/tpe2-g6-server-2023.2Q-bin.tar.gz" "$temp_dir/"
+cp "$client_dir/tpe2-g6-client-2023.2Q-bin.tar.gz" "$temp_dir/"
+cd "$temp_dir"
+
+# Server
+tar -xzf "tpe2-g6-server-2023.2Q-bin.tar.gz"
+chmod +x tpe2-g6-server-2023.2Q/run-server
+sed -i -e 's/\r$//' tpe2-g6-server-2023.2Q/*.sh
+rm "tpe2-g6-server-2023.2Q-bin.tar.gz"
+
+# Client
+tar -xzf "tpe2-g6-client-2023.2Q-bin.tar.gz"
+chmod +x tpe1-g6-client-2023.2Q/*-cli
+sed -i -e 's/\r$//' tpe2-g6-client-2023.2Q/*.sh
+rm "tpe2-g6-client-2023.2Q-bin.tar.gz"
